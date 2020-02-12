faa9f70c16142c986a97c5575e9f863758e59467
Scott <scott@pollen.com.au>
2020-02-13 09:30:21 +1100

    ignore temp

--- src/.temp/config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  "trailingSlash": true,
-  "pathPrefix": "",
-  "titleTemplate": "%s - Scott Blissett - Designer/Developer",
-  "siteUrl": "https://www.absentees.co",
-  "version": "0.7.12"
-}
--- src/.temp/plugins-server.js
+++
@@ -1,8 +0,0 @@
-import plugin_gridsome_plugin_google_analytics_6 from "/Users/scottb/Development/absentees-co/node_modules/@gridsome/plugin-google-analytics/gridsome.client.js"
-
-export default [
-  {
-    run: plugin_gridsome_plugin_google_analytics_6,
-    options: {"id":"UA-55969548-1"}
-  }
-]
