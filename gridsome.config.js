e8c6d8a56e8b5260bf3a0d5cb77671dd0d398bc7
Scott <djlethal109@gmail.com>
2020-07-20 09:05:27 +1000

    adjustment to packages

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,37 +1,43 @@
-// This is where project configuration and plugin options are located. 
+// This is where project configuration and plugin options are located.
 // Learn more: https://gridsome.org/docs/config
 
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Scott Blissett - Designer/Developer',
-  siteUrl: 'https://www.absentees.co',
-  siteDescription: 'Digital designer/developer based in Sydney NSW.',
-  plugins: [
-    {
-      use: '@gridsome/source-filesystem',
-      options: {
-        typeName: 'Projects',
-        path: '_posts/project/**/*.md',
-      }
-    },
-    {
-      use: "gridsome-source-rss",
-      options: {
-        feedUrl: "https://www.instapaper.com/starred/rss/4947229/Id9po8tqCIjKH7uS41GkqTKs",
-        typeName: 'Articles',
-        // Parser options, see: https://www.npmjs.com/package/rss-parser
-        parser: {}
-      }
-    },
-    {
-      use: '@gridsome/plugin-google-analytics',
-      options: {
-        id: 'UA-55969548-1'
-      }
-    }
-  ]
-}
-
-
+    siteName: "Scott Blissett - Designer/Developer",
+    siteUrl: "https://www.absentees.co",
+    siteDescription: "Digital designer/developer based in Sydney NSW.",
+    plugins: [
+        {
+            use: "@gridsome/source-filesystem",
+            options: {
+                typeName: "Projects",
+                path: "_posts/project/**/*.md",
+            },
+        },
+        {
+            use: "gridsome-source-rss",
+            options: {
+                feedUrl:
+                    "https://www.instapaper.com/starred/rss/4947229/Id9po8tqCIjKH7uS41GkqTKs",
+                typeName: "Articles",
+                // Parser options, see: https://www.npmjs.com/package/rss-parser
+                parser: {},
+            },
+        },
+        {
+            use: `gridsome-plugin-netlify-cms`,
+            options: {
+                configPath: `static/admin/config.yml`,
+                htmlPath: "static/admin/index.html",
+            },
+        },
+        {
+            use: "@gridsome/plugin-google-analytics",
+            options: {
+                id: "UA-55969548-1",
+            },
+        },
+    ],
+};
