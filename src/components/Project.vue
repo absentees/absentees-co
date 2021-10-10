971987b256c42e20d5d99ee882006b0b93fe0a59
Scott B <djlethal109@gmail.com>
2021-10-10 17:59:55 +1100

    project page WIP

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,7 +1,16 @@
 <template>
   <article :id="project.node.id" class="project">
     <div class="description">
-      <h2>{{ project.node.title }}</h2>
+      <h2>
+        <template v-if="project.node.path">
+					<g-link :to="project.node.path">{{
+            project.node.title
+          }}</g-link>
+				</template>
+				<template v-else>
+					{{project.node.title}}
+				</template>
+      </h2>
       <span class="role">{{ project.node.role }}</span>
       <span v-html="project.node.content"></span>
     </div>
@@ -24,15 +33,15 @@ export default {
 
 <style lang="scss" scoped>
 .project {
-    img {
-        width:100%;
-    }
-    .role {
-        display: block;
-        margin-bottom: 0.5ch;
-        opacity: 0.6;
-    }
+  img {
+    width: 100%;
+  }
+  .role {
+    display: block;
+    margin-bottom: 0.5ch;
+    opacity: 0.6;
+  }
 
-    margin-bottom: 8ch;
+  margin-bottom: 8ch;
 }
 </style>
