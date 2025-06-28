22b89b0545d235441d6a92b198a52a3b11617cc3
Scott B <djlethal109@gmail.com>
2025-06-28 22:20:39 +1000

    Update package.json scripts to include NODE_OPTIONS for legacy OpenSSL support; refactor Hyperlinks.vue for improved image handling and transitions; add netlify.toml for build configuration.

--- src/pages/Hyperlinks.vue
+++ src/pages/Hyperlinks.vue
@@ -2,23 +2,24 @@
   <Layout>
     <p>
       Recent links saved on
-      <a target="_blank" href="https://www.are.na/absentees-_/web-0abin7eksxc"
-        >are.na</a
-      >
+      <a target="_blank" href="https://www.are.na/absentees-_/web-0abin7eksxc">are.na</a>
     </p>
     <ul>
-      <a
-        v-for="edge in $page.allBlock.edges"
-        :key="edge.node.id"
-        :href="edge.node.url"
-      >
-        <li>
-          <img :src="edge.node.image.url" />
-          <!-- <span>{{ edge.node.title }}</span> -->
-          
-        </li>
-      </a>
+      <li v-for="edge in $page.allBlock.edges" :key="edge.node.id">
+        <button @click="selectImage(edge.node)" class="thumbnail-button">
+          <img :src="edge.node.image.url" :alt="edge.node.title" />
+        </button>
+      </li>
     </ul>
+
+    <transition name="fade">
+      <div v-if="selectedImage" class="overlay" @click="closeImage">
+        <button class="close-button" @click="closeImage">x close</button>
+        <a :href="selectedImage.url" target="_blank" rel="noopener noreferrer" @click.stop>
+          <img :src="selectedImage.image.url" :alt="selectedImage.title" class="enlarged-image" />
+        </a>
+      </div>
+    </transition>
   </Layout>
 </template>
 
@@ -41,45 +42,104 @@ query {
 
 <script>
 export default {
-    metaInfo: {
-        title: "Hyperlinks",
-        meta: []
+  metaInfo: {
+    title: "Hyperlinks",
+    meta: []
+  },
+  data() {
+    return {
+      selectedImage: null
+    };
+  },
+  methods: {
+    selectImage(node) {
+      this.selectedImage = node;
+    },
+    closeImage() {
+      this.selectedImage = null;
     }
+  }
 };
 </script>
 
 <style lang="scss" scoped>
 ul {
   display: flex;
-  flex-direction: row;
   flex-wrap: wrap;
   column-gap: 0.5rem;
+  padding: 0;
+  list-style: none;
+}
 
-  a {
-    box-sizing: border-box;
-    // width: 100%;
-    margin-bottom: 0.5rem;    
+li {
+  margin-bottom: 0.5rem;
+}
 
-    &:hover {
-      img {
-        width: 400px;
-      }
-    }
+.thumbnail-button {
+  padding: 0;
+  border: none;
+  background: transparent;
+  cursor: pointer;
+
+  img {
+    display: block;
+    width: 15px;
+    margin: 0 0.25rem;
+    transition: transform 0s ease-out;
   }
 
-  li {
-    text-align: center;
-    margin: 0;
-
-    img {
-      width: 15px;
-      margin: 0 0.25rem;
-    }
+  &:hover img {
+    transform: scale(1.2);
   }
 }
 
-img {
+.overlay {
+  position: fixed;
+  z-index: 100;
+  top: 0;
+  left: 0;
   width: 100%;
-  max-width: 200px;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.8);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.enlarged-image {
+  display: block;
+  width: 400px;
+  max-width: 90vw;
+  max-height: 90vh;
+  box-shadow: 0 0 40px rgba(0, 0, 0, 0.5);
+}
+
+.close-button {
+  position: absolute;
+  top: 1rem;
+  right: 1rem;
+  border: none;
+  color: white;
+  font-size: 1rem;
+  cursor: pointer;
+  padding: 0.25rem 0.5rem;
+  background-color: rgba(255, 255, 255, 0.05);
+  border-radius: 0.15rem;
+  transition: background-color 200ms ease;
+  line-height: 1.41em;
+
+  &:hover {
+    background-color: rgba(255, 255, 255, 0.1);
+  }
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0s ease;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
